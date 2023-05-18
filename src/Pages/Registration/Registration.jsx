60bcb6f0da2fb15fbebf01725863677095e39830
unknown <shuvrodevmondal337@gmail.com>
2023-05-18 08:55:10 +0600

    added login and registration design

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,9 +1,47 @@
 
 const Registration = () => {
     return (
-        <div>
-            <h2>This is registration page</h2>
+        <div className="hero  ">
+        <div className="hero-content flex-col gap-10">
+          <div className="text-center text-white bg-blue-300 rounded-2xl shadow-2xl p-4">
+            <h1 className="text-5xl font-bold">Welcome to Toy Troopers!! <br /> Register Here!</h1>
+          </div>
+          <div className="card shadow-2xl  w-full">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" placeholder="Your Name" className="input input-info" required/>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" placeholder="email" className="input input-info" required/>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" placeholder="password" className="input input-info" required/>
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input type="text" placeholder="Your Photo URL" className="input input-info" required/>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn border-none rounded-3xl bg-emerald-400 hover:bg-emerald-500">Login</button>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
